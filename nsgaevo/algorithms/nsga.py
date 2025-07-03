9e2312443d7d0f671ff8d2e7d69ab6a76ccbb268
Elijah Ang <elijah.anghw@gmail.com>
2025-07-03 14:28:16 +0200

    Clean up code

--- nsgaevo/algorithms/nsga.py
+++ nsgaevo/algorithms/nsga.py
@@ -181,10 +181,6 @@ def objective_functions(drone):
     for prop in drone.props:
         points.append(prop["loc"])
     points.append([0, 0, 0])    # Include origin
-    # points.append([0.045, 0, 0.1]) # Include points for drone main body
-    # points.append([-0.045, 0, 0.1]) # Include points for drone main body
-    # points.append([0, 0.045, 0.1]) # Include points for drone main body
-    # points.append([0, -0.045, 0.1]) # Include points for drone main body
     points = np.array(points)
     hull = ConvexHull(points[:,0:2])
     vol = hull.volume
@@ -194,8 +190,6 @@ def objective_functions(drone):
 
     if sim.hover_status == "ST":
         return [sim.hover_status, sim.alpha, min(sim.eig_m), vol]
-        # maneuverability = np.exp(np.mean(np.log(sim.eig_m)))
-        # return [sim.hover_status, sim.alpha, maneuverability, vol]
 
     elif sim.hover_status == "SP":
         return [sim.hover_status, sim.alpha, 0, vol]
